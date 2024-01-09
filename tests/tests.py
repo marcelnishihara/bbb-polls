71320e84d9abe346fac697dce41403be30e92ee7
Marcel Nishihara <marcelnishihara@gmail.com>
2024-01-09 15:56:22 -0300

    v2024_01_09: Second round of tests

--- tests/tests.py
+++ tests/tests.py
@@ -9,14 +9,19 @@ COUNTER = 0
 
 while True:
     POLLS = [
-        '/2024/01/05/bbb-24---enquete-uol-ate-agora-qual-dos-participantes-anunciados-e-o-seu-preferido.htm',
-        '/2024/01/07/bbb-24---enquete-uol-qual-das-6-mulheres-voce-quer-ver-entre-participantes.htm',
-        '/2024/01/07/bbb-24---enquete-uol-quem-voce-quer-ver-entre-os-participantes.htm'
+        [
+            '/2024/01/05/bbb-24---enquete-uol-ate-agora-qual-dos-participantes-anunciados-e-o-seu-preferido.htm',
+            '/2024/01/07/bbb-24---enquete-uol-qual-das-6-mulheres-voce-quer-ver-entre-participantes.htm',
+            '/2024/01/07/bbb-24---enquete-uol-quem-voce-quer-ver-entre-os-participantes.htm'
+        ],
+        [
+            '/2024/01/08/bbb-24---enquete-uol-com-qual-dos-participantes-voce-teve-menos-afinidade.htm'
+        ]
     ]
 
     COUNTER += 1
 
-    for poll_endpoint in POLLS:
+    for poll_endpoint in POLLS[1]:
         URL = 'http://0.0.0.0:8080'
         HEADERS = { 'Poll-Endpoint': poll_endpoint }
 
@@ -27,7 +32,7 @@ while True:
             timeout=10
         )
 
-        print(f'Request #{COUNTER} for {poll_endpoint}, Status Code: {RESPONSE.status_code}\n\n')
+        print(f'Request #{COUNTER} for {poll_endpoint}, Status Code: {RESPONSE.status_code}')
         sleep(120)
-    
-    sleep(1440)
+
+    sleep(900)
