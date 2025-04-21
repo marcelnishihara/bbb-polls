5f7f9930af9a7f787d440f32d23484ab16fd8b7a
Marcel Nishihara <marcelnishihara@gmail.com>
2025-04-21 17:03:06 -0300

    v2025: Final knock-out poll

--- tests/log_join.py
+++ tests/log_join.py
@@ -24,7 +24,7 @@ log_files_data = list(map(
     log_files
 ))
 
-poll_endpoint = '/2025/03/10/bbb-25---enquete-uol---8-paredao.htm'
+poll_endpoint = '/2025/03/31/bbb-25---enquete-uol-quem-voce-quer-eliminar-no-paredao.htm'
 
 log_files_data = list(filter(
     lambda data: 'url' in data and poll_endpoint in data['url'] and data['players'] != [],
