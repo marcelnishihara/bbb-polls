a3b39cec8043cfbafb51a31479f49f2f7e57c58f
Marcel Nishihara <marcelnishihara@gmail.com>
2026-03-29 04:02:08 -0300

    v2026: Tenth knock-out poll. X (Twitter) API under review.

--- tests/tests.py
+++ tests/tests.py
@@ -59,14 +59,14 @@ class Client:
 if __name__ == '__main__':
     today_is = Helpers.datetime()
     poll_key = 'paredao'
-    poll_index = 49
+    poll_index = 52
     counter = 0
 
     while True:
         try:
             create_tweet = False
             tweet_conditions = (
-                (counter % 12 == 0)
+                (counter % 6 == 0)
             )
 
             if tweet_conditions:
